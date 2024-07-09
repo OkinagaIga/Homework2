cef06a5ca08bc6723df2e8ba4aa9618c923dc6c7
OkinagaIga <okinagaiga@gmail.com>
2024-07-09 10:59:17 +0300

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,4 @@
+homework_quantity = 12
+hours_spent = 1.5
+course_name = 'Python,'
+print('Курс:',course_name,'всего задач:',homework_quantity,', затрачено часов:',hours_spent,', среднее время выполнения:', hours_spent / homework_quantity, 'часа')
